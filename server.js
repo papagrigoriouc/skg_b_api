b81c7420f266eb78753a80aabd205cbc4138bb8f
Chris <papagrigoriou_c@kalia.gr>
2022-03-04 23:57:27 +0200

    server init

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-var http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello World!');
-}).listen(8080);
+const app = express(); 
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/user', (req, res, next)=>{
+    res.send('<h1>User:' + req.body.username + '</h1>');
+});
+
+app.get('/', (req, res, next )=>{
+    res.send(
+        '<form action="/user"   method="POST"><input type="text" name="username"><button type="submit">Create User</button> </form>');
+        
+    });
+
+app.listen(5000);
