91b5cae09d7ab4e47a9faf0b55731f61332a59cc
Chris <papagrigoriou_c@kalia.gr>
2022-03-11 11:26:32 +0200

    update server.js

--- routes/property.js
+++ routes/property.js
@@ -4,7 +4,23 @@ const Property = require('../models/property');
 
 router.get('/all', (req, res)=>{
     Property.find().then(properties => res.send(properties));
-})
+});
+
+router.post('/create',(req, res)=>{
+    
+    const title = req.body.propertytitle;
+    const price = req.body.price;
+    const description = req.body.description;
+    const image = req.body.image;
+    const address = req.body.address;
+    const city = req.body.city;
+    const cords = [req.body.coordinates];
+       
+    console.log(title);
+    // Property.create({
+    //     title, price, description, image, address, city, cords
+    // });
+});
 
 
 
--- server.js
+++ server.js
@@ -13,7 +13,29 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Routes
 app.use('/api/property', property);
 
-//Db config
-const con = mongoose.connect("mongodb://5.189.177.98:37017/realestate", {useNewUrlParser: true});
 
-app.listen(5000);
+//Db config
+const con = mongoose.connect("mongodb://5.189.177.98:37017/realestate", {useNewUrlParser: true})
+.then(()=>{
+    console.log('Database succesfully connected!')
+},
+error =>{
+    console.log('Could not connect to database: ' + error)
+});
+
+//Ports
+const port = 5000;
+const server = app.listen(port, ()=>{
+    console.log('Connected to port ' + port);
+});
+
+//404 error
+app.use ((req, res, next)=> {
+    next(createError(404));
+});
+
+app.use(function (err, req, res, next){
+    console.error(err.message);
+    if(!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+});
